46989faef6f27ea89a2ffaa53fdfbc114639ed30
DonRomirez <romk.plastinin@mail.ru>
2023-04-30 22:24:35 +0300

    first commit

---
+++ KonhoidNikomeda/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace KonhoidNikomeda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.KH_button = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.A_Box = new System.Windows.Forms.TextBox();
+            this.L_Box = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(554, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Конхоида Никомеда";
+            // 
+            // KH_button
+            // 
+            this.KH_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.KH_button.Location = new System.Drawing.Point(849, 477);
+            this.KH_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.KH_button.Name = "KH_button";
+            this.KH_button.Size = new System.Drawing.Size(219, 73);
+            this.KH_button.TabIndex = 3;
+            this.KH_button.Text = "Показать График";
+            this.KH_button.UseVisualStyleBackColor = true;
+            this.KH_button.Click += new System.EventHandler(this.KH_button_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(28, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "х = a+ l соs t, y = а tg t+l sin t";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(32, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "a=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(32, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "l=";
+            // 
+            // A_Box
+            // 
+            this.A_Box.Location = new System.Drawing.Point(69, 69);
+            this.A_Box.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.A_Box.Name = "A_Box";
+            this.A_Box.Size = new System.Drawing.Size(100, 22);
+            this.A_Box.TabIndex = 7;
+            this.A_Box.Text = "30";
+            // 
+            // L_Box
+            // 
+            this.L_Box.Location = new System.Drawing.Point(69, 112);
+            this.L_Box.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.L_Box.Name = "L_Box";
+            this.L_Box.Size = new System.Drawing.Size(100, 22);
+            this.L_Box.TabIndex = 8;
+            this.L_Box.Text = "40";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.Location = new System.Drawing.Point(32, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 25);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "←";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button2.Location = new System.Drawing.Point(125, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 25);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "→";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button3.Location = new System.Drawing.Point(96, 222);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 64);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "↑";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button4.Location = new System.Drawing.Point(96, 317);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(25, 64);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "↓";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(36, 428);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(64, 66);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "+";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(142, 431);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(64, 63);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "-";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button7.Location = new System.Drawing.Point(96, 291);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(24, 23);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "*";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Смещение осей:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(15, 406);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Смена масштаба:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::KonhoidNikomeda.Properties.Resources.pngwing_com;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(152, 152);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(120, 115);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::KonhoidNikomeda.Properties.Resources.pngwing_com__1_;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(278, 44);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(788, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1080, 561);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.L_Box);
+            this.Controls.Add(this.A_Box);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.KH_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "График Конхоиды";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button KH_button;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox A_Box;
+        private System.Windows.Forms.TextBox L_Box;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ KonhoidNikomeda/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KonhoidNikomeda
+{
+    public partial class Form1 : Form
+    {
+        Pen pen1, pen2, pen_ax;
+        double x0, y0, x1, y1,x2,y2, t, a, l, a_mult=1,l_mult=1;
+
+        Bitmap canvas;
+
+        Graphics user_Graphics;
+        Color user_color;
+        int w, h;
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+        }
+
+        Image shrek, osel, shrek_run;
+        public Form1()
+        {
+            InitializeComponent();
+            canvas = new Bitmap(ClientRectangle.Width, ClientRectangle.Height);
+            user_Graphics = Graphics.FromImage(canvas);
+            user_Graphics.FillRectangle(Brushes.White, 0, 0, ClientRectangle.Width, ClientRectangle.Height);
+            shrek = pictureBox1.Image;
+            shrek_run = Properties.Resources.pngwing_com;
+            osel = Properties.Resources.pngwing_com__2_;
+           // pictureBox1.Image = canvas;
+            w = pictureBox1.Width;
+            h = pictureBox1.Height;
+            x0 = w / 2;
+            y0 = h / 2;
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            user_color = Color.Red;
+            pen_ax = new Pen(Color.Blue, 1);
+            pen1 = new Pen(user_color, 3);
+            pen2 = new Pen(user_color, 3);
+            //pen2.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            
+        }
+        private void KH_button_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Graphics graph = this.pictureBox1.CreateGraphics();
+            System.Drawing.Rectangle rect = ClientRectangle;
+            System.Windows.Forms.PaintEventArgs e1 = new System.Windows.Forms.PaintEventArgs(graph, rect);
+            pictureBox1_Paint(sender, e1);
+            a = Convert.ToDouble(A_Box.Text);
+            l = Convert.ToDouble(L_Box.Text);
+            String sum_a_l =Convert.ToString(a + l);
+            double a_res = a * a_mult;
+            double l_res = l * a_mult;
+            user_Graphics.Clear(BackColor);
+            user_Graphics.DrawLine(pen_ax, (float)x0, 5, (float)x0, h);
+            user_Graphics.DrawLine(pen_ax, 0, (float)y0, w - 5, (float)y0);
+            user_Graphics.DrawLine(pen_ax, w - 10, (float)y0 + 5, w - 5, (float)y0);
+            user_Graphics.DrawLine(pen_ax, w - 10, (float)y0 - 5, w - 5, (float)y0);
+            user_Graphics.DrawLine(pen_ax, (float)x0 - 5, 10, (float)x0, 5);
+            user_Graphics.DrawLine(pen_ax, (float)x0 + 5, 10, (float)x0, 5);
+            string xAxisLabel = "X";
+            string yAxisLabel = "Y";
+            Font labelFont = new Font("Arial", 10, FontStyle.Regular);
+            Brush labelBrush = Brushes.Black;
+            PointF xAxisLabelPosition = new PointF(w - 10, (float)y0+5);
+            PointF yAxisLabelPosition = new PointF((float)x0 - 15, 10);
+            PointF aPosition= new PointF((float)x0 +(float)a_res, (float)y0 + 5);
+            PointF a_plus_l_Position = new PointF((float)x0 + (float)(a_res+l_res), (float)y0 + 5);
+            user_Graphics.DrawString(xAxisLabel, labelFont, labelBrush, xAxisLabelPosition);
+            user_Graphics.DrawString(yAxisLabel, labelFont, labelBrush, yAxisLabelPosition);
+            user_Graphics.DrawString(A_Box.Text, labelFont, labelBrush, aPosition);
+            user_Graphics.DrawString(sum_a_l, labelFont, labelBrush, a_plus_l_Position);
+
+            pictureBox1.Image = canvas;
+            x2 = x1;
+            y2 = y1;
+            t = -Math.PI / 2+0.1;
+            x1 =x0 + a_res + l_res * Math.Cos(t);
+            y1 =y0 + a_res * Math.Tan(t) + l_res * Math.Sin(t);
+            while (t<= 1.57 && x2 <= pictureBox1.Width && y2 > -1e+10)
+            {
+                x2 =x0+ a_res + l_res * Math.Cos(t);
+                y2 =y0+ a_res * Math.Tan(t) + l_res * Math.Sin(t);
+                if (y2 < -1e+10) break;
+                user_Graphics.DrawLine(pen1, (float)x1, (float)y1, (float)x2, (float)y2);
+                x1 = x2;
+                y1 = y2;
+                t += Math.PI / 180;
+            }
+            t = Math.PI / 2-0.1;
+            x1 =x0+ a_res + l_res * Math.Cos(t);
+            y1 =y0+ a_res * Math.Tan(t) + l_res * Math.Sin(t);
+            while (t <= 4.7&&x2 >= 0 && x2 <= pictureBox1.Width && y2 < 1e+10)
+            {
+                x2 =x0+ a_res + l_res * Math.Cos(t);
+                y2 =y0+ a_res * Math.Tan(t) + l_res * Math.Sin(t);
+                user_Graphics.DrawLine(pen2, (float)x1, (float)y1, (float)x2, (float)y2);
+                x1 = x2;
+                y1 = y2;
+                t += Math.PI / 180;
+            }
+            pictureBox1.Refresh();
+            pictureBox1.Image = canvas;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((x0 - 50) > 0)
+                x0 = x0 - 50;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((x0+50)<w)
+            x0 = x0+ 50;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if ((y0 - 50) >0)
+                y0 = y0 - 50;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if((y0+50)<h)
+            y0 = y0 + 50;
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            x0 = w / 2;
+            y0 = h / 2;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            a_mult /= 1.2;
+            l_mult /= 1.2;
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            a_mult *= 1.2;
+            l_mult *= 1.2;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = shrek;
+        }
+        private void pictureBox2_MouseEnter(object sender, EventArgs e)
+        {
+        }
+    }
+}
